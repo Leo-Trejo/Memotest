056fb0d03651fa4ead9c2054e0a34956864a6e2d
LMT <Cheis0.0Brock@gmail.com>
2023-11-27 02:36:32 -0300

    Juego memotest sin cypress

---
+++ memotest.js
@@ -0,0 +1,137 @@
+$contenedorCartas = document.getElementById("contenedor-cartas")
+$btnComenzarJuego = document.getElementById("boton-mezclar")
+$contenedorGanador = document.getElementById("ganaste")
+$contenedorJuego = document.getElementById("contenedor-juego")
+
+let partida = 0
+const cartas = [...$contenedorCartas.children]
+
+let cartasReveladas = {
+    cartaPrimaria: "",
+    cartaSecundaria:""
+}
+
+const volverAJugar= ()=>{
+        $contenedorCartas.style.display = "none";
+        $contenedorGanador.style.display = "flex";
+        $btnComenzarJuego.textContent = "VOLVER A JUGAR"
+        $btnComenzarJuego.style.width = "300px"
+}
+
+const limpiarCartasReveladas = ()=>{
+    cartasReveladas.cartaPrimaria = ""
+    cartasReveladas.cartaSecundaria = ""
+}
+
+
+const mezclarCartas = ()=>{
+    function numerosAleatorios() {
+        return Math.random() - 0.5;
+    }
+    cartas.sort(numerosAleatorios)
+}
+
+const posicionarCartasAleatoriamente=()=>{
+    while ($contenedorCartas.firstChild) {
+        $contenedorCartas.removeChild($contenedorCartas.firstChild);
+    }
+    cartas.forEach((carta)=>{
+        $contenedorCartas.appendChild(carta)
+    })
+}
+
+const ocultarCartas = ()=>{
+    const imgCartas = cartas.map(carta=> carta.querySelector("img"))
+    imgCartas.forEach((img)=>{
+        img.style.opacity = 0
+    })
+}
+const revelarCarta=(e)=>{
+    cartaSeleccionada = e.target
+    cartaSeleccionada.style.opacity = 1
+    if(cartasReveladas.cartaPrimaria === ""){
+        cartasReveladas.cartaPrimaria = cartaSeleccionada
+    }else{
+        cartasReveladas.cartaSecundaria = cartaSeleccionada
+    }
+    if(cartasReveladas.cartaSecundaria !== ""){
+        comprobarCartas()
+        setTimeout(()=>{
+         comprobarJuegoTerminado()   
+        },1000)
+    }
+}
+
+const otorgarRevelarCarta = ()=>{
+    cartas.forEach((carta)=>{
+        carta.addEventListener("click",revelarCarta)
+    })
+}
+const borrarCarta = ()=>{
+    cartas.forEach((carta)=>{
+        let cartacomparar = carta.querySelector("img")
+        let srcCarta = cartacomparar.src
+        
+        if(srcCarta.length === cartasReveladas.cartaPrimaria.src.length && cartacomparar.id === cartasReveladas.cartaPrimaria.id){
+            carta.style.opacity = 0
+        }
+        else if(srcCarta.length === cartasReveladas.cartaSecundaria.src.length && cartacomparar.id === cartasReveladas.cartaSecundaria.id){
+            carta.style.opacity = 0
+        }
+        
+    })
+}   
+
+const comprobarCartas = ()=>{
+    const laMismaCarta = (cartasReveladas.cartaPrimaria === cartasReveladas.cartaSecundaria)
+    const cartasIguales = (cartasReveladas.cartaPrimaria.src === cartasReveladas.cartaSecundaria.src && cartasReveladas.cartaPrimaria.id !== cartasReveladas.cartaSecundaria.id)
+    
+    if(laMismaCarta){
+        cartasReveladas.cartaSecundaria = ""
+        return
+    }
+    if (cartasIguales) {
+        setTimeout(()=>{
+            borrarCarta()
+        },400)
+        setTimeout(()=>{
+            limpiarCartasReveladas()
+        },500)
+        
+    }else{
+        setTimeout(()=>{
+            limpiarCartasReveladas()
+            ocultarCartas()
+        },500)
+    }
+}
+const comprobarJuegoTerminado = () => {
+    let todasReveladas = true;
+
+    cartas.forEach((carta) => {
+        let opacidad = window.getComputedStyle(carta).getPropertyValue("opacity");
+        
+        if (opacidad !== "0") {
+            todasReveladas = false;
+            return;
+        }
+    });
+
+    if (todasReveladas) {
+        volverAJugar()
+    }
+};
+
+
+const comenzarJuego = ()=>{
+    if(partida === 1){
+        location.reload()
+    }
+    partida++
+    ocultarCartas()
+    mezclarCartas()
+    posicionarCartasAleatoriamente()
+    otorgarRevelarCarta()
+}
+
+$btnComenzarJuego.addEventListener("click",comenzarJuego)
